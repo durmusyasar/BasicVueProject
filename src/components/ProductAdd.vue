4eaab7b6366177e9d368a20c21074679c2115d69
Durmuş YAŞAR <durmusyasar13@gmail.com>
2020-12-14 10:48:07 +0300

    add project files

---
+++ src/components/ProductAdd.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="addForm">
+    <form @submit.prevent="handleAdd">
+      <div class="form-group">
+        <label for="id">Id</label>
+        <input type="text" v-model="product.id" class="form-control" id="id"/>
+      </div>
+      <div class="form-group">
+        <label for="categoryId">Category Id</label>
+        <input type="text" v-model="product.categoryId" class="form-control" id="categoryId"/>
+      </div>
+      <div class="form-group">
+        <label for="productName">Product Name</label>
+        <input type="text" v-model="product.productName" class="form-control" id="productName"/>
+      </div>
+      <div class="form-group">
+        <label for="quantityPerUnit">Quantity Per Unit</label>
+        <input type="text" v-model="product.quantityPerUnit" class="form-control" id="quantityPerUnit"/>
+      </div>
+      <div class="form-group">
+        <label for="unitPrice">Unit Price</label>
+        <input type="text" v-model="product.unitPrice" class="form-control" id="unitPrice"/>
+      </div>
+      <div class="form-group">
+        <label for="unitsInStocks">Units In Stocks</label>
+        <input type="text" v-model="product.unitsInStocks" class="form-control" id="unitsInStocks"/>
+      </div>
+      <button class="btn btn-sm btn-success" type="submit">Ekle</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "product-add",
+  data() {
+    return {
+      product: {
+        id: '',
+        categoryId: '',
+        productName: '',
+        quantityPerUnit: '',
+        unitPrice: '',
+        unitsInStocks: '',
+      },
+    };
+  },
+  methods:{
+      handleAdd(){
+          this.$emit("add:product", this.product)
+      }
+  }
+};
+</script>
+<style scoped>
+#addForm {
+    margin: 100px;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="list">
+    <p v-if="products.length == 0">Ürün Listesi Boş</p>
+    <table v-else class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Ürün Adı</th>
+          <th>Kategori</th>
+          <th>Açıklama</th>
+          <th>Birim Fiyatı</th>
+          <th>Stok Adedi</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.id"
+              type="text"
+              class="form-control"
+              id="id"
+            />
+          </td>
+          <td v-else>{{ product.id }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.productName"
+              type="text"
+              class="form-control"
+              id="productName"
+            />
+          </td>
+          <t v-else>{{ product.productName }}</t>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.categoryId"
+              type="text"
+              class="form-control"
+              id="categoryId"
+            />
+          </td>
+          <td v-else>{{ product.categoryId }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.quantityPerUnit"
+              type="text"
+              class="form-control"
+              id="quantityPerUnit"
+            />
+          </td>
+          <td v-else>{{ product.quantityPerUnit }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.unitPrice"
+              type="text"
+              class="form-control"
+              id="unitPrice"
+            />
+          </td>
+          <td v-else>{{ product.unitPrice }}</td>
+          <td v-if="updateId === product.id">
+            <input
+              v-model="product.unitsInStocks"
+              type="text"
+              class="form-control"
+              id="unitsInStocks"
+            />
+          </td>
+          <td v-else>{{ product.unitsInStocks }}</td>
+          <td v-if="updateId !== product.id">
+            <button
+              class="btn btn-sm btn-primary mr-1"
+              @click="handleUpdate(product)"
+            >
+              Güncelle
+            </button>
+            <button
+              class="btn btn-sm btn-danger"
+              @click="handleDelete(product)"
+            >
+              Sil
+            </button>
+          </td>
+          <td v-else>
+            <button
+              class="btn btn-sm btn-primary mr-1"
+              @click="handleSave(product)"
+            >
+              Kaydet
+            </button>
+            <button
+              class="btn btn-sm btn-danger"
+              @click="updateId=null"
+            >
+              İptal
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product-list",
+  props: {
+    products: Array,
+  },
+  methods: {
+    handleDelete(product) {
+      this.$emit("delete:product", product);
+    },
+    handleUpdate(product) {
+      this.updateId = product.id;
+    },
+    handleSave(product){
+        this.$emit("save:product", product),
+        this.updateId = null
+    }
+  },
+  data() {
+    return { updateId: null };
+  },
+};
+</script>
+
+<style scoped>
+#list{
+  margin: 100px;
+}
+</style>>
